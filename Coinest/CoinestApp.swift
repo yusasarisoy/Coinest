c81a63df65d6e16a5e4d6ded6d62f0259e27d554
Yuşa Sarısoy <yusasarisoy@icloud.com>
2022-07-09 20:21:48 +0300

    Initial Commit

---
+++ Coinest/CoinestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinestApp.swift
+//  Coinest
+//
+//  Created by Şevval on 9.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoinestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
