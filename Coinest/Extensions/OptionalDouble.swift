ed2c76bfe96ac4f2b5bf1883f4b3978bbd97e1c9
Yuşa Sarısoy <yusasarisoy@icloud.com>
2022-09-23 18:51:54 +0300

    Add coin model as per API response of CoinGecko

---
+++ Coinest/Extensions/OptionalDouble.swift
@@ -0,0 +1,16 @@
+//
+//  OptionalDouble.swift
+//  Coinest
+//
+//  Created by Yuşa on 23.09.2022.
+//
+
+extension Optional where Wrapped == Double {
+  var orZero: Double {
+    self ?? 0
+  }
+  
+  var toInt: Int {
+    Int(self.orZero)
+  }
+}
---
+++ Coinest/Preview Content/Preview Documentation.swift
@@ -0,0 +1,51 @@
+//
+//  Preview Documentation.swift
+//  Coinest
+//
+//  Created by Yuşa on 23.09.2022.
+//
+
+// MARK: - CoinGecko API
+/*
+
+ API Endpoint:
+ https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h
+
+ JSON Response:
+ {
+     "id": "bitcoin",
+     "symbol": "btc",
+     "name": "Bitcoin",
+     "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+     "current_price": 18717.93,
+     "market_cap": 357775228080,
+     "market_cap_rank": 1,
+     "fully_diluted_valuation": 392164179695,
+     "total_volume": 33734565363,
+     "high_24h": 19475.03,
+     "low_24h": 18641.21,
+     "price_change_24h": -232.35889239136668,
+     "price_change_percentage_24h": -1.22615,
+     "market_cap_change_24h": -5825248847.010986,
+     "market_cap_change_percentage_24h": -1.6021,
+     "circulating_supply": 19158506,
+     "total_supply": 21000000,
+     "max_supply": 21000000,
+     "ath": 69045,
+     "ath_change_percentage": -73.00688,
+     "ath_date": "2021-11-10T14:24:11.849Z",
+     "atl": 67.81,
+     "atl_change_percentage": 27385.05085,
+     "atl_date": "2013-07-06T00:00:00.000Z",
+     "roi": null,
+     "last_updated": "2022-09-23T14:59:10.845Z",
+     "sparkline_in_7d": {
+       "price": [
+         19877.977087031624,
+         19694.477379430067,
+         19532.43085061006
+       ]
+     },
+     "price_change_percentage_24h_in_currency": -1.2261495372372209
+   }
+ */
