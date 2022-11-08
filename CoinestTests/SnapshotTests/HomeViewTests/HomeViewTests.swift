d9bc03f7d07363abb07096af6de8d5a8e6c2a149
Yuşa Sarısoy <yusasarisoy@icloud.com>
2022-11-08 18:38:01 +0300

    Add a snapshot testing for HomeView

---
+++ CoinestTests/SnapshotTests/HomeViewTests/HomeViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewTests.swift
+//  CoinestTests
+//
+//  Created by Yuşa on 8.11.2022.
+//
+
+@testable import Coinest
+import SnapshotTesting
+import SwiftUI
+import XCTest
+
+final class HomeViewTests: XCTestCase {
+  private let coin = DeveloperPreview.instance.coin
+  private var viewController: UIViewController!
+
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    let homeViewModel = HomeViewModel()
+    let homeView = HomeView()
+      .environmentObject(homeViewModel)
+    viewController = UIHostingController(rootView: homeView)
+  }
+
+  override func tearDownWithError() throws {
+    viewController = nil
+    try super.tearDownWithError()
+  }
+
+  func testHomeViewOniPhone() throws {
+    assertSnapshot(
+      matching: viewController,
+      as: .image(on: .iPhone13Mini)
+    )
+  }
+}
